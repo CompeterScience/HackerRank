2a7721045a941e7fff910c331c33ab142a56c728
CompeterScience <39387779+CompeterScience@users.noreply.github.com>
2018-05-17 14:24:51 -0700

    Create DiagonalDifference.java

---
+++ DiagonalDifference.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    /*
+     * Complete the diagonalDifference function below.
+     */
+    static int diagonalDifference(int[][] a) {
+        /*
+         * Write your code here.
+         */
+        int dimA = a.length;
+        int mainDiag = 0;
+        int antiDiag = 0;
+        for (int i = 0; i < dimA; i++) {
+            mainDiag += a[i][i];
+            antiDiag += a[i][dimA -1 -i];
+        }
+        int diagDif = Math.abs( mainDiag - antiDiag );
+        return diagDif;
+    }
+
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(scan.nextLine().trim());
+
+        int[][] a = new int[n][n];
+
+        for (int aRowItr = 0; aRowItr < n; aRowItr++) {
+            String[] aRowItems = scan.nextLine().split(" ");
+
+            for (int aColumnItr = 0; aColumnItr < n; aColumnItr++) {
+                int aItem = Integer.parseInt(aRowItems[aColumnItr].trim());
+                a[aRowItr][aColumnItr] = aItem;
+            }
+        }
+
+        int result = diagonalDifference(a);
+
+        bw.write(String.valueOf(result));
+        bw.newLine();
+
+        bw.close();
+    }
+}
